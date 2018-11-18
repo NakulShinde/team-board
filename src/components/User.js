1a21b1e2d15ecf314fc7568a0d722e9180981f97
NakulShinde <nakulshinde.it@gmail.com>
2018-11-18 11:34:48 +0530

    Minor css changes

--- src/components/User.js
+++ src/components/User.js
@@ -2,16 +2,28 @@ import React from 'react'
 
 import styles from './User.module.scss'
 
+const UserRole = (props) => {
+    return <div>
+        {props.userRole}
+        {(props.userRole.toLowerCase() === 'external') && <span className={styles.astricMark}>*</span>}
+    </div>
+}
 const User = (props) => {
     return <div className={styles.teamMember}>
         <div className={styles.memberAvatar}>
-            <img className={styles.image} alt="avatar" src="static/images/avatar-default.png"></img>
-            <div data-tooltip="Remove" className={styles.memberRemove} onClick={() => props.removeMember(props.user.id)}>
+            <img
+                className={styles.image}
+                alt="avatar"
+                src="static/images/avatar-default.png"></img>
+            <div
+                data-tooltip="Remove"
+                className={styles.memberRemove}
+                onClick={() => props.removeMember(props.user.id)}>
                 <span className={styles.memberRemoveSpan}>x</span>
             </div>
         </div>
         <div className={styles.memberDetails}>
-            <div>{props.user.role}</div>
+            <UserRole userRole={props.user.role}></UserRole>
             <div className={styles.memberUsername}>{props.user.username}</div>
         </div>
     </div>
